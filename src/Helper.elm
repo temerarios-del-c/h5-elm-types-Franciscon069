f3eed2c6b179d7fb96499d7ef7c5b64ed8029fa5
Francisco069 <jvasillo300@gmail.com>
2026-03-01 20:44:20 -0600

    correccion de programa

--- src/Helper.elm
+++ src/Helper.elm
@@ -1,72 +1,5 @@
 module Helper exposing (..)
 
-import Browser
-import Html exposing (..)
-import Html.Attributes exposing (src)
-import Html.Events exposing (onClick)
-
-
-
--- CARRUSEL (tu código original)
-
-
-type alias Model =
-    { indice : Int }
-
-
-init : Model
-init =
-    { indice = 0 }
-
-
-imagenes : List String
-imagenes =
-    [ "https://tse4.mm.bing.net/th/id/OIP.OvQNCbYUN7c16CAQKIsa2QHaEK?pid=Api&P=0&h=180"
-    , "https://tse1.mm.bing.net/th/id/OIP.e6Zw_XQi4cyq0jgJ12j2xwHaEK?pid=Api&P=0&h=180"
-    ]
-
-
-imagenActual : Int -> String
-imagenActual indice =
-    Maybe.withDefault "" (List.head (List.drop indice imagenes))
-
-
-type Msg
-    = Siguiente
-    | Anterior
-
-
-update : Msg -> Model -> Model
-update msg model =
-    case msg of
-        Siguiente ->
-            { model | indice = modBy (List.length imagenes) (model.indice + 1) }
-
-        Anterior ->
-            { model | indice = modBy (List.length imagenes) (model.indice - 1) }
-
-
-view : Model -> Html Msg
-view model =
-    div []
-        [ button [ onClick Anterior ] [ text "←" ]
-        , img [ src (imagenActual model.indice) ] []
-        , button [ onClick Siguiente ] [ text "→" ]
-        ]
-
-
-main : Program () Model Msg
-main =
-    Browser.sandbox
-        { init = init
-        , update = update
-        , view = view
-        }
-
-
-
--- CALIFICACIONES (requerido por TestSuite)
-
 
 type Grade
     = Approved
@@ -74,31 +7,28 @@ type Grade
     | Pending
 
 
-categoricalGrade : List Float -> List Grade
-categoricalGrade list =
-    List.map
-        (\grade ->
-            if grade >= 7 then
-                Approved
-
-            else if grade >= 0 then
-                Failed
-
-            else
-                Pending
-        )
-        list
-
-
-
--- VUELOS (requerido por TestSuite)
-
-
 type AirplaneStatus
     = Cancelled
     | Delayed
     | OnTime
     | Boarding
+
+
+categoricalGrade : List Float -> List Grade
+categoricalGrade grades =
+    List.map gradeFromFloat grades
+
+
+gradeFromFloat : Float -> Grade
+gradeFromFloat grade =
+    if grade >= 7 then
+        Approved
+
+    else if grade >= 0 then
+        Failed
+
+    else
+        Pending
 
 
 airplaneScheduleAction : AirplaneStatus -> String
@@ -118,5 +48,5 @@ airplaneScheduleAction status =
 
 
 airportAction : List AirplaneStatus -> List String
-airportAction list =
-    List.map airplaneScheduleAction list
+airportAction statuses =
+    List.map airplaneScheduleAction statuses
